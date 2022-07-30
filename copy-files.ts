6e84f3bc81b92127834a89e6c985f5fcdb13a956
Aluneed <31174087+aluneed@users.noreply.github.com>
2022-07-30 19:53:56 +0800

    update

---
+++ copy-files.ts
@@ -0,0 +1,6 @@
+import { copy, emptyDir } from "https://deno.land/std@0.148.0/fs/mod.ts";
+
+console.log(await Deno.stat("./"));
+emptyDir("./docs")
+copy("./dist/static-blog", "./docs", { overwrite: true });
+console.log("success");
