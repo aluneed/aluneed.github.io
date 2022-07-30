51b36fb3c7fa5c4f5ef38cb109d5e22945dba0f0
Aluneed <31174087+aluneed@users.noreply.github.com>
2022-07-30 20:01:52 +0800

    copy 404.html for github pages

--- copy-files.ts
+++ copy-files.ts
@@ -2,5 +2,6 @@ import { copy, emptyDir } from "https://deno.land/std@0.148.0/fs/mod.ts";
 
 console.log(await Deno.stat("./"));
 emptyDir("./docs")
+copy("./dist/static-blog/index.html", "./dist/static-blog/404.html", { overwrite: true });
 copy("./dist/static-blog", "./docs", { overwrite: true });
 console.log("success");
