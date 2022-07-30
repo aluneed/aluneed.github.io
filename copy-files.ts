4adc34b9145d0466e8a0060ca39f385936fc9d5e
Aluneed <31174087+aluneed@users.noreply.github.com>
2022-07-30 23:01:39 +0800

    script bug fix

--- copy-files.ts
+++ copy-files.ts
@@ -2,7 +2,7 @@ import { copy, emptyDir } from "https://deno.land/std@0.148.0/fs/mod.ts";
 
 console.log(await Deno.stat("./"));
 emptyDir("./docs")
-copy("./dist/static-blog/index.html", "./dist/static-blog/404.html", { overwrite: true });
-copy("./dist/static-blog", "./docs", { overwrite: true });
-copy("./CNAME", "./docs", { overwrite: true });
+await copy("./dist/static-blog/index.html", "./dist/static-blog/404.html", { overwrite: true });
+await copy("./dist/static-blog", "./docs", { overwrite: true });
+await copy("./CNAME", "./docs", { overwrite: true });
 console.log("success");
