e0926f738fa291ac522d5923ba662363c831d2be
aluneed <aluneed@users.noreply.github.com>
2022-08-01 12:09:15 +0000

    Apply automatic changes

--- src/app/content-index.ts
+++ src/app/content-index.ts
@@ -1,4 +1,4 @@
 export const contentIndex = [
   {"id":"0","title":"blog test","date":"2022-07-24","tags":"blog, test","category":"blog","type":"blog","path":"blog-test.md"},
-  {"id":"1","title":"","date":"2022-07-24","tags":"blog, test","category":"blog","type":"blog","path":"finally-this-shit-starts-running.md"}
+  {"id":"1","title":"终于, 这玩意儿跑起来了","date":"2022-08-01","tags":"frontend, angular, deno, typescript, blog, web","category":"blog","type":"blog","path":"finally-this-shit-starts-running.md"}
 ]
