0d766367c75e926945ec0e8a062c2e60469e5bd1
aluneed <aluneed@users.noreply.github.com>
2022-08-02 09:15:41 +0000

    Apply automatic changes

--- src/app/tags.ts
+++ src/app/tags.ts
@@ -4,11 +4,10 @@ export interface Tag {
 }
 
 export const tags: Tag[] = [
-  {"name":"blog","count":2},
-  {"name":"test","count":1},
   {"name":"frontend","count":1},
   {"name":"angular","count":1},
   {"name":"deno","count":1},
   {"name":"typescript","count":1},
+  {"name":"blog","count":1},
   {"name":"web","count":1},
 ]
